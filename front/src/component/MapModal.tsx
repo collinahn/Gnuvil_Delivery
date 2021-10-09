2003ea23a24b27097fad95426397e9cd614351b8
Collin Ahn <collinahn@hufs.ac.kr>
2021-10-10 01:09:16 +0900

    [Modify] 맵이 최초 실행시 즉각적으로 뜨지 않던 문제 수정
    map tag가 완성되기 전 map을 load하여 오류가 발생할 것으로 추정하여 autoload=false설정으로 script 완성 이후 콜백되도록 작성해두었는데 이런 구조에서는 맵 최초 로드시 로드가 안되거나 오래걸리는 오류가 있어서
    index.html에서 미리 script로 포함을 시켜놓고 로드한다.

--- front/src/component/MapModal.tsx
+++ front/src/component/MapModal.tsx
@@ -16,137 +16,118 @@ interface MapModalProps {
   history: History;
 }
 
-const API_KEY = `${process.env.REACT_APP_KAKAO_MAP_KEY}`;
-const API_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${API_KEY}&libraries=services&autoload=false`;
-
 const MapModal: React.FC<MapModalProps> = ({ history }) => {
-  // const { history } = props;
   const [location, setLocation] =
     useState<string>("지도에서 위치를 선택해주세요");
 
-  // url로 로드하는 스크립트 ts에서 사용할 수 있도록
+  // index.html에서 로드하는 스크립트 네임스페이스 ts에서 사용할 수 있도록
   const kakao = (window as any).kakao;
 
   useEffect(() => {
-    // script 및 tag가 완성되기 전 map을 만들게 되어 오류가 발생함.
-    // autoload=false 선언으로 onload함수가 script완성 이후 콜백되도록 작성
-    const kakaoMapScript = document.createElement("script");
+    kakao?.maps.load(() => {
+      let container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
+      let options = {
+        center: new kakao.maps.LatLng(37.5959, 127.0587), //지도의 중심좌표.
+        level: 5, //지도의 레벨(확대, 축소 정도)
+      };
 
-    // 카카오맵 소스, 키는 .env파일에 저장
-    kakaoMapScript.src = API_URL;
+      let map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
 
-    document.head.appendChild(kakaoMapScript);
+      // 주소-좌표 변환 객체를 생성
+      let geocoder = new kakao.maps.services.Geocoder();
 
-    kakaoMapScript.onload = () => {
-      kakao?.maps.load(() => {
-        let container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
-        let options = {
-          center: new kakao.maps.LatLng(37.5959, 127.0587), //지도의 중심좌표.
-          level: 5, //지도의 레벨(확대, 축소 정도)
-        };
-
-        let map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-
-        // 주소-좌표 변환 객체를 생성
-        let geocoder = new kakao.maps.services.Geocoder();
-
-        // 마커 생성
-        let marker = new kakao.maps.Marker({
-          map: map,
-          draggable: true,
-          position: map.getCenter(),
-        });
-        let infowindow = new kakao.maps.InfoWindow({ zindex: 1 });
-
-        // 지도 드래깅 이벤트를 등록한다 (드래그 시작 : dragstart, 드래그 종료 : dragend)
-        kakao.maps.event.addListener(map, "drag", function () {
-          let message = "중심 좌표: " + map.getCenter().toString();
-          console.log(message);
-        });
-
-        // 지도를 클릭하면 마지막 파라미터로 넘어온 함수 호출
-        kakao.maps.event.addListener(map, "click", function (mouseEvent: any) {
-          // 클릭한 위도, 경도 정보
-          let latlng = mouseEvent.latLng;
-
-          // 마커 위치를 클릭한 위치로
-          marker.setPosition(latlng);
-
-          let message = "onClick\nLat: " + latlng.getLat() + ", \n";
-          message += "Lng: " + latlng.getLng();
-
-          let resultDiv = document.getElementById("clickLatlng");
-          if (resultDiv) resultDiv.innerHTML = message;
-          console.log(message);
-        });
-
-        if (navigator.geolocation) {
-          // GeoLocation을 이용해서 접속 위치 확인
-          navigator.geolocation.getCurrentPosition(function (position) {
-            let lat = position.coords.latitude; // 위도
-            let lon = position.coords.longitude; // 경도
-
-            // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성
-            let locPosition = new kakao.maps.LatLng(lat, lon);
-
-            map.setCenter(locPosition);
-          });
-        } else {
-          // HTML5의 GeoLocation을 사용할 수 없을때
-          let locPosition = new kakao.maps.LatLng(37.5959, 127.0587);
-          map.setCenter(locPosition);
-          alert("GPS정보를 사용할 수 없습니다.");
-        }
-
-        //  지도 좌측 상단 현재 지도 중심좌표로 주소 표시
-        // searchAddrFromCoords(map.getCenter(), displayCenterInfo);
-
-        // 지도를 클릭했을 때 클릭 위치 좌표에 대한 주소정보를 표시하도록 이벤트를 등록
-        // TODO 타입 정보를 올바르게 표시하기 2021.10.08
-        kakao.maps.event.addListener(map, "click", function (mouseEvent: any) {
-          searchDetailAddrFromCoords(
-            mouseEvent.latLng,
-            function (result: any, status: any) {
-              if (status === kakao.maps.services.Status.OK) {
-                //api단에서 도로명주소로 변환할 수 없을 떄가 있는듯? - 현재는 사용안함
-                var detailAddr = !!result[0].road_address
-                  ? "<div>도로명주소 : " +
-                    result[0].road_address.address_name +
-                    "</div>"
-                  : "";
-                detailAddr +=
-                  "<div>지번 주소 : " +
-                  result[0].address.address_name +
-                  "</div>";
-
-                var content =
-                  '<div class="MapModal-address">' +
-                  '<span class="title">주소정보</span>' +
-                  detailAddr +
-                  "</div>";
-
-                // 마커를 클릭한 위치에 표시
-                marker.setPosition(mouseEvent.latLng);
-                marker.setMap(map);
-
-                // 인포윈도우에 법정동 상세 주소정보를 표시
-                // infowindow.setContent(content);
-                // infowindow.open(map, marker);
-
-                setLocation(result[0].address.address_name);
-              }
-            }
-          );
-        });
-
-        function searchDetailAddrFromCoords(coords: any, callback: any) {
-          // 좌표로 법정동 상세 주소 정보를 요청
-          geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
-        }
+      // 마커 생성
+      let marker = new kakao.maps.Marker({
+        map: map,
+        draggable: true,
+        position: map.getCenter(),
       });
-    };
+      let infowindow = new kakao.maps.InfoWindow({ zindex: 1 });
 
-    return () => kakaoMapScript.remove();
+      // 지도 드래깅 이벤트를 등록한다 (드래그 시작 : dragstart, 드래그 종료 : dragend)
+      kakao.maps.event.addListener(map, "drag", function () {
+        let message = "중심 좌표: " + map.getCenter().toString();
+        console.log(message);
+      });
+
+      // 지도를 클릭하면 마지막 파라미터로 넘어온 함수 호출
+      kakao.maps.event.addListener(map, "click", function (mouseEvent: any) {
+        // 클릭한 위도, 경도 정보
+        let latlng = mouseEvent.latLng;
+
+        // 마커 위치를 클릭한 위치로
+        marker.setPosition(latlng);
+
+        let message = "onClick\nLat: " + latlng.getLat() + ", \n";
+        message += "Lng: " + latlng.getLng();
+
+        let resultDiv = document.getElementById("clickLatlng");
+        if (resultDiv) resultDiv.innerHTML = message;
+        console.log(message);
+      });
+
+      if (navigator.geolocation) {
+        // GeoLocation을 이용해서 접속 위치 확인
+        navigator.geolocation.getCurrentPosition(function (position) {
+          let lat = position.coords.latitude; // 위도
+          let lon = position.coords.longitude; // 경도
+
+          // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성
+          let locPosition = new kakao.maps.LatLng(lat, lon);
+
+          map.setCenter(locPosition);
+        });
+      } else {
+        // HTML5의 GeoLocation을 사용할 수 없을때
+        let locPosition = new kakao.maps.LatLng(37.5959, 127.0587);
+        map.setCenter(locPosition);
+        alert("GPS정보를 사용할 수 없습니다.");
+      }
+
+      //  지도 좌측 상단 현재 지도 중심좌표로 주소 표시
+      // searchAddrFromCoords(map.getCenter(), displayCenterInfo);
+
+      // 지도를 클릭했을 때 클릭 위치 좌표에 대한 주소정보를 표시하도록 이벤트를 등록
+      // TODO 타입 정보를 올바르게 표시하기 2021.10.08
+      kakao.maps.event.addListener(map, "click", function (mouseEvent: any) {
+        searchDetailAddrFromCoords(
+          mouseEvent.latLng,
+          function (result: any, status: any) {
+            if (status === kakao.maps.services.Status.OK) {
+              //api단에서 도로명주소로 변환할 수 없을 떄가 있는듯? - 현재는 사용안함
+              var detailAddr = !!result[0].road_address
+                ? "<div>도로명주소 : " +
+                  result[0].road_address.address_name +
+                  "</div>"
+                : "";
+              detailAddr +=
+                "<div>지번 주소 : " + result[0].address.address_name + "</div>";
+
+              var content =
+                '<div class="MapModal-address">' +
+                '<span class="title">주소정보</span>' +
+                detailAddr +
+                "</div>";
+
+              // 마커를 클릭한 위치에 표시
+              marker.setPosition(mouseEvent.latLng);
+              marker.setMap(map);
+
+              // 인포윈도우에 법정동 상세 주소정보를 표시
+              // infowindow.setContent(content);
+              // infowindow.open(map, marker);
+
+              setLocation(result[0].address.address_name);
+            }
+          }
+        );
+      });
+
+      function searchDetailAddrFromCoords(coords: any, callback: any) {
+        // 좌표로 법정동 상세 주소 정보를 요청
+        geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
+      }
+    });
   }, []);
 
   const goBack = (history: History, e: React.MouseEvent) => {
